801fbc7bd4e39f13126b210b08541184f78e80d1
Rishab Ramanathan <rrishab2001@gmail.com>
2023-02-27 13:58:34 +0530

    ass1/static_t and static_nt branch prediction

---
+++ ass1/code/static_t.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *file;
+    file = fopen("../traces/trace_01","r");
+    char buff[255];
+    char ch;
+    int i = 0;
+    int taken = 0;
+    while (fgetc(file) != EOF)
+    {
+        fgets(buff,255,file);
+        if(buff[8] == 49)//buff[8] = taken/not taken of branch address
+            taken++;
+        i++;
+    }
+    fclose(file);
+    printf("Success:\t%d\n",taken);
+    printf("Total:\t\t%d\n",i);
+    printf("Accuracy:\t%f\n",(taken*1.0/i));
+}
