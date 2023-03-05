e2514b2eecf3008e07ba5d55df40b84e22e56702
Rishab Ramanathan <rrishab2001@gmail.com>
2023-03-05 19:40:42 +0530

    dynamic t branch prediction

--- ass1/code/dynamic_last_taken.c
+++ ass1/code/dynamic_last_taken.c
@@ -43,8 +43,8 @@ int main()
 {
     FILE *file;
     file = fopen("../traces/trace_01","r");
-    int i, j, addr, taken, predicted_taken;
-    i = j = addr = taken = predicted_taken = 0;
+    int i, j, addr, taken, predicted_taken, total_taken;
+    i = j = addr = taken = predicted_taken = total_taken = 0;
     branch_node *start = (branch_node *) malloc (sizeof(branch_node));
     branch_node *current = (branch_node *) malloc (sizeof(branch_node));
     branch_node *temp = (branch_node *) malloc (sizeof(branch_node));
@@ -54,6 +54,7 @@ int main()
         fscanf(file,"%x %d",&addr, &taken);
         if(i == 0)
         {
+            predicted_taken = 1;
             start = createlist(addr, taken);
             current = start;
         }
@@ -62,25 +63,28 @@ int main()
             temp = search(start,addr);
             if (temp == NULL)
             {
+                predicted_taken = 1;
                 current = push(current, addr, taken);
 
                 //printf("new entry: %x\t%d\n", current->addr, current->taken);
             }
             else
             {
+                predicted_taken = temp->taken;
                 //printf("%d duplicate, updating\n", i);
                 temp->taken = taken;
             }
         }
+        if(predicted_taken == taken)
+            total_taken++;
         i++;
     }
+
+    printf("Success:\t%d\n",total_taken);
+    printf("Total:\t\t%d\n",i);
+    printf("Accuracy:\t%f\n",(total_taken*1.0/i));
     current = start;
     i=0;
     fclose(file);
-    while (current != NULL)
-    {
-        //printf("%d. %x\t%d\n",i++,current->addr,current->taken);
-        current = current->next;
-    }
     
 }
