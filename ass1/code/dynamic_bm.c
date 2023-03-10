d9f5dbc2350d7202d038e050f3b39624a1b06cf1
Rishab Ramanathan <rrishab2001@gmail.com>
2023-03-10 20:55:54 +0530

    file renaming/comments

--- ass1/code/dynamic_bm.c
+++ ass1/code/dynamic_bm.c
@@ -5,10 +5,10 @@
 typedef struct branch_node
 {
     unsigned int lsb_addr;
-    int taken_state;
+    int taken_state; //0-1-2-3
 }branch_node;
 
-int search(unsigned int lsb_addr, branch_node *arr, int arr_size)
+int search(unsigned int lsb_addr, branch_node *arr, int arr_size)//linear search
 {
     for (int i = 0; i < arr_size; i++)
     {
@@ -23,41 +23,38 @@ int main()
     FILE *file;
     file = fopen("../traces/trace_01","r");
 
-    unsigned int addr, temp;
+    unsigned int addr, temp, lsb_addr;
     int taken, n, i, j, top_index, total_correct_pred, default_state, prediction;
-    addr = temp = taken = i = j = top_index = total_correct_pred = 0;
+    addr = temp = lsb_addr = taken = i = j = top_index = total_correct_pred = 0;
     default_state = 1;
-    n = 30;
-    int arr_size = (int)pow(2,n);
+    n = 24;
+    int arr_size = (int)pow(2,n);//2^n unique counters
     branch_node *arr = (branch_node *) malloc(sizeof(branch_node) * arr_size);
 
     while (fscanf(file,"%x %d",&addr, &taken) != EOF)
     {
-        unsigned int lsb_addr = (addr<<(32-n))>>(32-n); //least significant n bits
-        //printf("%x - ",lsb_addr);
+        lsb_addr = (addr<<(32-n))>>(32-n); //least significant n bits
         i = search(lsb_addr, arr, top_index);
         if (i == -1)
         {
-            //printf("new addition : %x at %d\n",lsb_addr,top_index);
             arr[top_index].lsb_addr = lsb_addr;
-            arr[top_index++].taken_state = taken+1;
+            arr[top_index++].taken_state = taken+1;//converting to 2 bit state 
+            //(0 becomes 1 (weakly not taken) and 1 becomes 2(weakly taken))
             prediction = default_state;//prediction
         }
         else
         {
             //prediction
-            if(arr[i].taken_state > 1)
+            if(arr[i].taken_state > 1) //2-3 taken
                 prediction = 1;
-            else
+            else //0-1 not taken
                 prediction = 0;
             //updation
             if(taken == 1)
-                arr[i].taken_state = (++arr[i].taken_state > 3)?3:arr[i].taken_state;
+                arr[i].taken_state = (++arr[i].taken_state > 3)?3:arr[i].taken_state; //saturated increment
             else
-                arr[i].taken_state = (--arr[i].taken_state < 0)?0:arr[i].taken_state;
-            //printf("duplicate found at %x\n",lsb_addr);
+                arr[i].taken_state = (--arr[i].taken_state < 0)?0:arr[i].taken_state; //saturated decrement
         }
-        //printf("%d|\t %x %d %d %d\n",j++,lsb_addr,prediction,taken,total_correct_pred);
         if(prediction == taken)
             total_correct_pred++;
         j++;
